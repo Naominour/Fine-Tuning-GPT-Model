8862acc390a2957398f62cc304561d60214cb35c
Naomi <naeimehnour@gmail.com>
2024-10-10 15:45:44 +0100

    CausalSelfAttention added

--- train_gpt2.py
+++ train_gpt2.py
@@ -5,6 +5,28 @@ import torch.nn as nn
 
 
 #-----------------------------------------------------------
+# Multi-Head attention function
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_ambd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+        
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+
+
 # Feedforward function
 class MLP(nn.Module):
 
