5a576eb9ad83ddebd1b6edbd99246424ce333ef8
Naomi <naeimehnour@gmail.com>
2024-10-10 11:13:39 +0100

    FeedForward Function Added

--- train_gpt2.py
+++ train_gpt2.py
@@ -5,13 +5,22 @@ import torch.nn as nn
 
 
 #-----------------------------------------------------------
+# Feedforward function
+class MLP(nn.Module):
 
-class MLP(self, config):
-    super().__init__()
-    
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GLUE(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
 
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj
+        return x
 
-
+# Attention block function
 class Block(nn.Module):
 
     def __init__(self, config):
