8d1686c6f6252bf95f443b9eed955823c7fea0da
Naomi <naeimehnour@gmail.com>
2024-10-11 14:52:09 +0100

    prefix tokens added

--- train_gpt2.py
+++ train_gpt2.py
@@ -154,4 +154,30 @@ max_length = 30
 model = GPT.from_pretrained('gpt2')
 model.eval()
 model.to('cuda')
-print("The program did't crash!")
+
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens.to('cuda')
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_indices, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
+
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decode = enc.decode(tokens)
+    print(">", decode)
