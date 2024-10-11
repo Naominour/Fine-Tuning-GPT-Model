02c4362c0a68efdc412a79abe84ba56c84858953
Naomi <naeimehnour@gmail.com>
2024-10-11 15:31:37 +0100

    Device Autodection added

--- train_gpt2.py
+++ train_gpt2.py
@@ -149,6 +149,13 @@ class GPT(nn.Module):
         return model
 
 #------------------------------------------------------------------------------------
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+print(f"using device: {device}")
+
 num_return_sequences = 5
 max_length = 30
 
