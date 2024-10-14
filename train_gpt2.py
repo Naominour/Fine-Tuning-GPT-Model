6081c5deaf23cbad82010393318467d2438b1b08
Naomi <naeimehnour@gmail.com>
2024-10-14 10:20:09 +0100

    Init params

--- train_gpt2.py
+++ train_gpt2.py
@@ -91,6 +91,17 @@ class GPT(nn.Module):
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
 
+        # init params
+        self.apply(self.__init__weights)
+
+    def __init__weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
 
     def forward(self, idx, targets=None):
         B, T = idx.size()
