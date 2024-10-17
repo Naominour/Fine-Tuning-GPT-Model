130043db80ed3ae0ff6952022d33d29f433bd37f
Naomi <naeimehnour@gmail.com>
2024-10-17 09:05:20 +0100

    tokens shard added

--- train_gpt2.py
+++ train_gpt2.py
@@ -11,9 +11,65 @@ import time
 import torch
 import math
 import torch.nn as nn
+import pandas as np
 import os 
 
 #-----------------------------------------------------------
+local_dir = "edu_fineweb10B"
+remote_name = "sample-10BT"
+shard_size = int(1e8)
+
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
+
+# init the tokenizer
+enc = tiktoken.get_encoding("gpt2")
+eot = enc._special_tokens['<|endoftext|>']
+def tokenize(doc):
+    tokens = [eot]
+    tokens.extend(enc.encode_ordinary(doc["text"]))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all()
+    tokens_np_unit16 = tokens_np.astype(np.unit16)
+    return tokens_np_unit16
+
+def write_datafile(filename, tokens_np):
+    with open(filename, "wb") as f:
+        f.write(tokens_np.tobytes())
+
+nprocs = max(1, os.cpu_count()//2)
+with mp.Pool(nprocs) as pool:
+    shared_index = 0
+    all_token_np = np.empty((shard_size,), dtype=np.uint16)
+    token-count = 0
+    progress_bar = None
+    for tokens in pool.imap(tokenize, fw, chunksize=16):
+        if token_count + len(tokens) < shard_size:
+            all_token_np[token_count:token_count+len(tokens)] = tokens
+            token_count += len(tokens)
+
+            if progress_bar is None:
+                progress_bar = tqdm(total=shared_size, unit="okens", desc=f"Shard {shared_index:06d}")
+            progress_bar.update_len(tokens)
+        else:
+            split = "val" if shared_index == 0 else "train"
+            filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shared_index:06d}")
+            reminder = shared_size - token_count
+            progress_bar.update(reminder)
+            all_tokens_np[token_count:token_count_reminder] = tokens[:reminder]
+            write_datafile(filename, all_token_np)
+            shard_index += 1
+            progress_bar = None
+            all_tokens_np[0:len(tokens)-reminder] = tokens[reminder:]
+            token_count = len(tokens)-reminder
+
+    if token_count != 0:
+        split = "val" if shared_index == 0 else "train"
+        filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shared_index:06d}")
+        write_datafile(filename, all_token_np[:token_count])
+ 
 # Multi-Head attention function
 class CausalSelfAttention(nn.Module):
 
