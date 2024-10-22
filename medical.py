a3bbe671d2970e39e36c8dd17738161bf50ed49b
Naomi <naeimehnour@gmail.com>
2024-10-22 15:59:46 +0100

    extract shards

--- medical.py
+++ medical.py
@@ -13,25 +13,36 @@ shard_size = int(1e8)  # 100M tokens per shard, total of 100 shards
 DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
 os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
+# download the dataset
+fw = load_dataset("gamino/wiki_medical_terms")
+
+# Initialize the tokenizer outside of the main function
+enc = tiktoken.get_encoding("gpt2")
+eot = enc._special_tokens['<|endoftext|>']  # end of text token
+
+def tokenize(doc):
+    # tokenizes a single document and returns a numpy array of uint16 tokens
+    text = doc['page_text']
+
+    tokens = [eot]  # the special <|endoftext|> token delimits all documents
+    tokens.extend(enc.encode_ordinary(text))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
+
+def write_datafile(filename, tokens_np):
+    np.save(filename, tokens_np)
+
 def main():
-    # download the dataset
-    fw = load_dataset("gamino/wiki_medical_terms")
 
-    # init the tokenizer
-    enc = tiktoken.get_encoding("gpt2")
-    eot = enc._special_tokens['<|endoftext|>']  # end of text token
-    
-    def tokenize(doc):
-        # tokenizes a single document and returns a numpy array of uint16 tokens
-        tokens = [eot]  # the special <|endoftext|> token delimits all documents
-        tokens.extend(enc.encode_ordinary(doc["text"]))
-        tokens_np = np.array(tokens)
-        assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
-        tokens_np_uint16 = tokens_np.astype(np.uint16)
-        return tokens_np_uint16
+    train_dataset = fw['train']
+    train_dataset_dict = train_dataset.to_dict() 
 
-    def write_datafile(filename, tokens_np):
-        np.save(filename, tokens_np)
+    train_dataset_list = [
+        {key: train_dataset_dict[key][i] for key in train_dataset_dict}
+        for i in range(len(train_dataset_dict['page_text']))  # Use an existing column to determine length
+    ]
 
     # tokenize all documents and write output shards, each of shard_size tokens (last shard has remainder)
     nprocs = max(1, os.cpu_count() // 2)
@@ -41,7 +52,7 @@ def main():
         all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
         token_count = 0
         progress_bar = None
-        for tokens in pool.imap(tokenize, fw, chunksize=16):
+        for tokens in pool.imap(tokenize, train_dataset_list, chunksize=16):
             # is there enough space in the current shard for the new tokens?
             if token_count + len(tokens) < shard_size:
                 # simply append tokens to current shard
