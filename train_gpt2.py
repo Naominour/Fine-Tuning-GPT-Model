8619c55740ca803f836bd19aa3d0ccb09d912988
Naomi <119312416+Naominour@users.noreply.github.com>
2025-01-03 18:28:19 +0000

    Update train_gpt2.py

--- train_gpt2.py
+++ train_gpt2.py
@@ -11,7 +11,6 @@ import torch.distributed as dist
 
 from dataclasses import dataclass
 from torch.nn import functional as F
-from hellaswag import render_example, iterate_examples
 from torch.distributed import init_process_group, destroy_process_group
 from torch.nn.parallel import DistributedDataParallel as DDP
 from transformers import GPT2LMHeadModel
@@ -205,12 +204,14 @@ class GPT(nn.Module):
         return optimizer
 
 # -----------------------------------------------------------------------------
+# Load tokens from the specified file
 def load_tokens(filename):
     npt = np.load(filename)
-    npt = npt.astype(np.int32) # added after video
+    npt = npt.astype(np.int32)
     ptt = torch.tensor(npt, dtype=torch.long)
     return ptt
 
+# DataLoaderLite is a simplified loader for managing training and validation data
 class DataLoaderLite:
     def __init__(self, B, T, process_rank, num_processes, split):
         self.B = B
@@ -219,7 +220,7 @@ class DataLoaderLite:
         self.num_processes = num_processes
         assert split in {'train', 'val'}
 
-        # get the shard filenames
+        # Collect and sort data shard filenames for the selected split
         data_root = "/content/drive/MyDrive/Medical_LLM/medical_dataset_cache"
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
@@ -232,47 +233,25 @@ class DataLoaderLite:
         self.reset()
 
     def reset(self):
-        # state, init at shard zero
+        # Start processing from the first shard
         self.current_shard = 0
         self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
 
     def next_batch(self):
+        # Fetch a batch of data
         B, T = self.B, self.T
         buf = self.tokens[self.current_position : self.current_position+B*T+1]
-        x = (buf[:-1]).view(B, T) # inputs
-        y = (buf[1:]).view(B, T) # targets
+        x = (buf[:-1]).view(B, T) # # Input tokens
+        y = (buf[1:]).view(B, T) # Target tokens
         # advance the position in the tensor
         self.current_position += B * T * self.num_processes
-        # if loading the next batch would be out of bounds, advance to next shard
+        # Advance the pointer and load the next shard if necessary
         if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
             self.current_shard = (self.current_shard + 1) % len(self.shards)
             self.tokens = load_tokens(self.shards[self.current_shard])
             self.current_position = B * T * self.process_rank
         return x, y
-
-# -----------------------------------------------------------------------------
-# helper function for HellaSwag eval
-# takes tokens, mask, and logits, returns the index of the completion with the lowest loss
-
-def get_most_likely_row(tokens, mask, logits):
-    # evaluate the autoregressive loss at all positions
-    shift_logits = (logits[..., :-1, :]).contiguous()
-    shift_tokens = (tokens[..., 1:]).contiguous()
-    flat_shift_logits = shift_logits.view(-1, shift_logits.size(-1))
-    flat_shift_tokens = shift_tokens.view(-1)
-    shift_losses = F.cross_entropy(flat_shift_logits, flat_shift_tokens, reduction='none')
-    shift_losses = shift_losses.view(tokens.size(0), -1)
-    # now get the average loss just for the completion region (where mask == 1), in each row
-    shift_mask = (mask[..., 1:]).contiguous() # we must shift mask, so we start at the last prompt token
-    masked_shift_losses = shift_losses * shift_mask
-    # sum and divide by the number of 1s in the mask
-    sum_loss = masked_shift_losses.sum(dim=1)
-    avg_loss = sum_loss / shift_mask.sum(dim=1)
-    # now we have a loss for each of the 4 completions
-    # the one with the lowest loss should be the most likely
-    pred_norm = avg_loss.argmin().item()
-    return pred_norm
 
 # -----------------------------------------------------------------------------
 def generate_text(model, device, device_type, ddp_rank, phrase, num_return_sequences):
